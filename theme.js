03af32e31862202b3f73cccfdc7146fd7023ce2b
SuguruOoki <oki.work0527@gmail.com>
2022-02-05 15:11:25 +0900

    [ADD]first commit

---
+++ theme.js
@@ -0,0 +1,21 @@
+export default {
+  fonts: {
+    body: 'Roboto, sans-serif',
+    monospace: '"Roboto Mono", monospace',
+  },
+  colors: {
+    text: 'white',
+    background: 'black',
+    primary: 'blue',
+  },
+  styles: {
+	h1: {
+	  textTransform: 'uppercase',
+	  letterSpacing: '0.1em',
+	  color: 'red',
+	},
+	blockquote: {
+	  fontStyle: 'italic',
+	},
+      }
+}
